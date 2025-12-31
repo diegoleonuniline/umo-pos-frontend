c191121be14cc618dd084336146fff78f9b5c2b4
diegoleonuniline <desarrollos@uniline.mx>
2025-12-31 10:55:53 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // URL del backend en Heroku (CAMBIA ESTO por tu URL real)
-    API_URL: 'https://umo-pos-api-ab3e10552800.herokuapp.com',
+    API_URL: 'https://umo-pos-api-71a155958263.herokuapp.com/',
     
     // Logo UMO
     LOGO_URL: 'https://res.cloudinary.com/dstcnsu6a/image/upload/v1754346747/ChatGPT_Image_4_ago_2025_16_32_04_cpudux.png',
